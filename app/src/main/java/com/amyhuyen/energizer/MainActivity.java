c3452c6d3066c5189f4f987ae655e228ae891aba
amyhuyen98 <amyhuyen98@gmail.com>
2018-07-16 14:33:03 -0700

    Implemented creating new user (authentication side)

--- app/src/main/java/com/amyhuyen/energizer/MainActivity.java
+++ app/src/main/java/com/amyhuyen/energizer/MainActivity.java
@@ -1,17 +1,83 @@
 package com.amyhuyen.energizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-    master
-// comment
 
-    // comment
-login
+    // the views
+    @BindView (R.id.etEmail) EditText etEmail;
+    @BindView (R.id.etPassword) EditText etPassword;
+    @BindView (R.id.btnRegister) Button btnRegister;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        // bind the views
+        ButterKnife.bind(this);
+
+    }
+
+    private void registerUser(){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            // email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            // password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // register user
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                        if (task.isSuccessful()){
+                            // display successful registration message
+                            Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                        } else{
+                            Toast.makeText(MainActivity.this, "Could not register, please try agian", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @OnClick (R.id.btnRegister)
+    public void onRegisterClick(){
+        registerUser();
     }
 }
