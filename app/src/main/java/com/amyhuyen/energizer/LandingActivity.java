0feadbff64f372b48a6623c28a87eba430401b6e
Angela Foley <acfoley@fb.com>
2018-07-16 18:20:11 -0700

    Login working with profile fragment

--- app/src/main/java/com/amyhuyen/energizer/LandingActivity.java
+++ app/src/main/java/com/amyhuyen/energizer/LandingActivity.java
@@ -2,6 +2,7 @@ package com.amyhuyen.energizer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+
 public class LandingActivity extends AppCompatActivity {
 
     // the views
@@ -22,6 +24,8 @@ public class LandingActivity extends AppCompatActivity {
     @BindView (R.id.tvWelcome) TextView tvWelcome;
 
     private FirebaseAuth firebaseAuth;
+
+    //Amy is going to put a logout button in this activity. TODO - move logout button to profile fragment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,13 @@ public class LandingActivity extends AppCompatActivity {
         FirebaseUser user = firebaseAuth.getCurrentUser();
         tvWelcome.setText("Welcome " + user.getEmail());
 
+        // Fragment setup
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        //add new fragment to our frame layout fl_profile
+        ft.add(R.id.fl_profile, new ProfileFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 
     // on click listener for logout button
@@ -63,5 +74,8 @@ public class LandingActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         finish();
         startActivity(intent);
+
+
+
     }
 }
