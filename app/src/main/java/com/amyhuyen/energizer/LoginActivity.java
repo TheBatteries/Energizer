8383a40d8ef1c93f6dcf2bf39c44a84b92133596
Angela Foley <acfoley@fb.com>
2018-07-19 18:13:43 -0700

    Started making separate landing activites/fragments and getUserType(), but needs work

--- app/src/main/java/com/amyhuyen/energizer/LoginActivity.java
+++ app/src/main/java/com/amyhuyen/energizer/LoginActivity.java
@@ -11,10 +11,18 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amyhuyen.energizer.models.Opportunity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,6 +38,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    private FirebaseUser currentFirebaseUser;
+    private DatabaseReference mDBUserRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,9 +50,12 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        mDBUserRef = FirebaseDatabase.getInstance().getReference().child("User");
+        currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
 //        // check if user already is logged in (if so, launch landing activity)
 //        if (firebaseAuth.getCurrentUser() != null){
+            //check user type here
 //            Intent intent = new Intent(getApplicationContext(), LandingActivity.class);
 //            finish();
 //            startActivity(intent);
@@ -52,6 +65,9 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void userLogin(){
+
+        //ADD method for deciding usertype
+
         String email = etEmail.getText().toString().trim();
         String password = etPassword.getText().toString().trim();
 
@@ -80,6 +96,7 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Logged In Successfully", Toast.LENGTH_SHORT).show();
 
                             // intent to landing activity
+                            //TODO - change intents according to UserType
                             Intent intent = new Intent(getApplicationContext(), VolunteerLandingActivity.class);
                             finish();
                             startActivity(intent);
@@ -104,5 +121,36 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         finish();
         startActivity(intent);
+    }
+
+    public String getUserType() {
+        String userType;
+        final String userId = currentFirebaseUser.getUid();
+        final HashMap<String, HashMap<String, String>> mapping = new HashMap<>();
+
+        mDBUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //TODO - most of this is pulled from elsewhere
+
+                mapping.putAll((HashMap<String, HashMap<String, String>>) dataSnapshot.getValue());
+
+                // iterate through mapping and create and add opportunities
+                for (String UserID: mapping.keySet()) {
+                    String UserType = new Opportunity(mapping.get(oppId).get("Name"), mapping.get(oppId).get("Description"), oppId);
+                    //not pointing to what I want, but similar logic
+                }
+
+            }
+
+        //get the list of Volunteers
+        //check to see if any element in VolunteersList matches the userId
+        //if yes, return volunteer. else, return NPO
+
+
+
+
+
+        return userType;
     }
 }
--- app/src/main/java/com/amyhuyen/energizer/NPOLandingActivity.java
+++ app/src/main/java/com/amyhuyen/energizer/NPOLandingActivity.java
@@ -1,13 +1,83 @@
 package com.amyhuyen.energizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.ButterKnife;
 
 public class NPOLandingActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser currentFirebaseUser;
+    private DatabaseReference mDBUserRef;
+    private String userType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_npo_landing);
+        setContentView(R.layout.activity_volunteer_landing);
+
+        // bind the views
+        ButterKnife.bind(this);
+
+        //get info on current user
+        firebaseAuth = firebaseAuth.getInstance();
+        currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDBUserRef = FirebaseDatabase.getInstance().getReference().child("User");
+
+        // check to see if user is already logged in
+        if (firebaseAuth.getCurrentUser() == null) {
+
+            // intent to login activity
+            Intent intent = new Intent(this, LoginActivity.class);
+            finish();
+            startActivity(intent);
+        }
+
+        //instantiate fragments
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment volunteerProfileFragment = new VolunteerProfileFragment();
+        final Fragment npoProfileFragment = new NpoProfileFragment();
+        final Fragment opportunityFeedFrag = new OpportunityFeedFragment();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.ic_action_profile:
+
+                        //TODO - when respective landings for volunteer and non-profit have been created, move the onNavegationSelected - volunteer icon chosen to that landing. then, user respective fragment
+
+//                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                            fragmentTransaction.replace(R.id.flContainer, npoProfileFragment).commit(); //use NPOProfileFragment
+//                            return true;
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.flContainer, volunteerProfileFragment).commit(); //user volunteerProfileFragment
+                        return true;
+
+                    case R.id.ic_action_feed:
+                        //TODO - add if else to check Volunteer or NPO, and take them to respective feed
+                        fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.flContainer, opportunityFeedFrag).commit();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
