df2ee87ffc044f2673d807407010fcb3a1102826
amyhuyen98 <amyhuyen98@gmail.com>
2018-08-02 10:05:27 -0700

    Removed hard-coded keys in SetCausesActivity

--- app/src/main/java/com/amyhuyen/energizer/SetCausesActivity.java
+++ app/src/main/java/com/amyhuyen/energizer/SetCausesActivity.java
@@ -155,7 +155,7 @@ public class SetCausesActivity extends AppCompatActivity {
                                 // create hashmap for UserID
                                 final HashMap<String, String> userIdDataMap = new HashMap<String, String>();
                                 // put UserID into the hashmap
-                                userIdDataMap.put("UserID", userId);
+                                userIdDataMap.put(DBKeys.KEY_USER_ID, userId);
                                 // push the hashmap to the preexisting database skill
                                 firebaseData.child(DBKeys.KEY_USERS_PER_CAUSE).child(userCauses.get(index).getCause()).push().setValue(userIdDataMap);
                                 // get the skill object ID from the database
